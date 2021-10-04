b7f3625d025bd49fa10314751b916885f47ef327
digital-programmer <mohandibya123@gmail.com>
2021-10-04 23:37:33 +0530

    Assets setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const port = 8000;
 
 const app = express();
@@ -6,13 +7,17 @@ const app = express();
 // use express router to connect
 app.use("/", require("./routes"));
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "assets")));
+
 // set view engine and view route
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.listen(port, function (err) {
   if (err) {
-    console.log("Error listening on port" + port);
+    console.log("Error listening on port", port);
   }
-  console.log("Server listening on port" + port);
+  console.log("Server listening on port", port);
 });
