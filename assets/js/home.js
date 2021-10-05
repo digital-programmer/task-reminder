c0829fe6e9572a5c578fc0dbcef52851e0fb6488
digital-programmer <mohandibya123@gmail.com>
2021-10-05 22:45:40 +0530

    Setup html css and js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,18 @@
 console.log("Javascript is running");
+
+// Program to set min date of input date to today
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = "0" + dd;
+}
+
+if (mm < 10) {
+  mm = "0" + mm;
+}
+
+today = yyyy + "-" + mm + "-" + dd;
+document.getElementById("due_date").setAttribute("min", today);
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,21 @@
 module.exports.home = function (req, res) {
+  let tasksList = [
+    {
+      id: "1",
+      description: "Annual report submission deadline",
+      category: "school",
+      due_date: "21 Oct, 2021",
+    },
+    {
+      id: "2",
+      description: "Get grocery",
+      category: "personal",
+      due_date: "02 Nov, 2021",
+    },
+  ];
+
   return res.render("home", {
     title: "To-Do App",
+    task_list: tasksList,
   });
 };
